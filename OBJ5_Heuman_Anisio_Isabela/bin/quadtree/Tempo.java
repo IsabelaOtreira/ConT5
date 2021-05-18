9d3604fc450b8ef5f37256a99d31456051f694fb
heumankeker <49781802+heumankeker@users.noreply.github.com>
2021-05-18 19:53:38 -0300

    Add files via upload

---
+++ OBJ5_Heuman_Anisio_Isabela/bin/quadtree/Tempo.java
@@ -0,0 +1,17 @@
+public class Tempo {
+	
+	public int TempoCome;
+	public int TempoFin;
+	
+	public void ContaTemp () {
+		TempoCome = (int) System.currentTimeMillis();				
+	}
+	
+	public void ParaTemp() {
+		
+		TempoFin = (int) System.currentTimeMillis();
+		System.out.println("Time : " + ((TempoFin - TempoCome)));
+		
+	}
+
+}
